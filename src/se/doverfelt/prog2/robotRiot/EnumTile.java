bcb5fe51cff57dc376a67d4f757c679eb9578f33
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2015-11-18 13:11:12 +0100

    Began working on RobotRiot

---
+++ src/se/doverfelt/prog2/robotRiot/EnumTile.java
@@ -0,0 +1,11 @@
+package se.doverfelt.prog2.robotRiot;
+
+/**
+ * Filnamn: EnumTile.java
+ *
+ * @author Rickard Doverfelt
+ *         Datum: 2015-11-18
+ */
+public enum EnumTile {
+    FLOOR, WALL, ROBOT;
+}
---
+++ src/se/doverfelt/prog2/robotRiot/RobotRiot.java
@@ -0,0 +1,53 @@
+package se.doverfelt.prog2.robotRiot;
+
+import java.util.Scanner;
+
+/**
+ * Filnamn: RobotRiot.java
+ *
+ * @author Rickard Doverfelt
+ *         Datum: 2015-11-18
+ */
+public class RobotRiot {
+
+    public static EnumTile[][] tiles;
+
+    public RobotRiot(int n, int m, String[] lines) {
+        parseTiles(n, m, lines);
+    }
+
+    private void parseTiles(int n, int m, String[] lines) {
+        tiles = new EnumTile[n][m];
+        for (int i = 0; i < n; i++) {
+            String[] chars = lines[i].split("");
+            for (int j = 0; j < m; j++) {
+                switch (chars[j]) {
+                    case ".":
+                        tiles[i][j] = EnumTile.FLOOR;
+                        System.out.println("FLOOR");
+                        break;
+                    case "#":
+                        tiles[i][j] = EnumTile.WALL;
+                        System.out.println("WALL");
+                        break;
+                    case "X":
+                        tiles[i][j] = EnumTile.ROBOT;
+                        System.out.println("ROBOT");
+                        break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        s.nextLine();
+        String[] lines = new String[n];
+        for (int i = 0; i < n; i++) {
+            lines[i] = s.nextLine();
+        }
+        new RobotRiot(n, m, lines);
+    }
+}
